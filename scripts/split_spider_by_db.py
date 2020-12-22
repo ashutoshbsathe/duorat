11e1f17e4898af54e81c514d0cd80df2deb97182
Vu Hoang <vuh@speak.ai>
2020-12-22 15:26:14 +1100

    [dev] - Fix split_spider_by_db.py

--- scripts/split_spider_by_db.py
+++ scripts/split_spider_by_db.py
@@ -32,18 +32,18 @@ def main(spider_path, duorat_path, aug_data="", aug_suffix="") -> None:
             json.dump([item], write_fp, indent=2)
 
     ### 2. Produce examples.json files
+    grouped_payload: Dict[str, List[dict]] = defaultdict(list)
     for examples_path in examples_paths:
         with open(os.path.join(spider_path, examples_path), "r") as read_fp:
             payload: List[dict] = json.load(read_fp)
 
-        grouped_payload: Dict[str, List[dict]] = defaultdict(list)
         for item in payload:
             db_id: str = item['db_id']
             grouped_payload[db_id].append(item)
 
-        for db_id, payload_group in grouped_payload.items():
-            with open(os.path.join(duorat_path, db_id, example_file_name), "wt+") as write_fp:
-                json.dump(payload_group, write_fp, indent=2)
+    for db_id, payload_group in grouped_payload.items():
+        with open(os.path.join(duorat_path, db_id, example_file_name), "wt") as write_fp:
+            json.dump(payload_group, write_fp, indent=2)
 
 
 if __name__ == '__main__':
