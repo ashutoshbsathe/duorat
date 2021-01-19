e19006e84da64d1ddf845de3a601f352b154481e
Vu Hoang <vuh@speak.ai>
2021-01-19 18:15:20 +1100

    Add extract_templates.py code (cont)

--- scripts/data_aug/extract_templates.py
+++ scripts/data_aug/extract_templates.py
@@ -13,7 +13,7 @@ def postprocess(sql: str) -> str:
         if ps != -1:
             pe = sql.find('"', ps + 1)
             if pe != -1:
-                sql = sql.replace(sql[ps + 1: pe], sql[ps + 1: pe].replace(".", "~"))
+                sql = sql.replace(sql[ps + 1: pe], sql[ps + 1: pe].replace(".", "~").replace("NULL", "!!!!").replace("AS", "!!"))
                 ps = pe + 1
             else:
                 break
