34adfd9b7c91d02f0e9b25ae705593d6bd4414b5
Vu Hoang <vuh@speak.ai>
2021-02-18 16:47:25 +1100

    Add get_testsuite_preds.py

---
+++ scripts/get_testsuite_preds.py
@@ -0,0 +1,30 @@
+import sys
+import json
+
+duorat_output_file = sys.argv[1]
+original_sparc_data_file = sys.argv[2]
+testsuite_output_file = sys.argv[3]
+
+fout_testsuite = open(testsuite_output_file, 'w')
+
+with open(original_sparc_data_file, "r") as f:
+    original_data = json.load(f)
+
+predictions = []
+with open(duorat_output_file, "r") as f:
+    for line in f:
+        line = line.strip()
+        entry_data = json.loads(line)
+        predictions.append(entry_data["inferred_code"])
+
+i = 0
+for example in original_data:
+    interaction = example["interaction"]
+
+    for pred in predictions[i:i+len(interaction)]:
+        fout_testsuite.write(f"{pred}\n")
+        i += 1
+    fout_testsuite.write("\n")
+
+fout_testsuite.flush()
+fout_testsuite.close()
