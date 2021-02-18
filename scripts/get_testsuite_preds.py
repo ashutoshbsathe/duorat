f9306427f291e622b40e2e99c6d6ae75aa8db597
Vu Hoang <vuh@speak.ai>
2021-02-18 16:57:53 +1100

    Minor fix

--- scripts/get_testsuite_preds.py
+++ scripts/get_testsuite_preds.py
@@ -11,11 +11,17 @@ with open(original_sparc_data_file, "r") as f:
     original_data = json.load(f)
 
 predictions = []
+count_empty_preds = 0
 with open(duorat_output_file, "r") as f:
     for line in f:
         line = line.strip()
         entry_data = json.loads(line)
-        predictions.append(entry_data["beams"][0]["inferred_code"])
+        if len(entry_data["beams"]) > 0:
+            predictions.append(entry_data["beams"][0]["inferred_code"])
+        else:
+            predictions.append('')
+            count_empty_preds += 1
+print(f"There are {count_empty_preds} empty predictions.")
 
 i = 0
 for example in original_data:
