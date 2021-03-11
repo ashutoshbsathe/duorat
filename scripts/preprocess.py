01aafde9fd10c2b209914af06e01ab58f6436868
Cong Duy Vu Hoang <vu.hoang@oracle.com>
2021-03-11 16:26:59 +1100

    Add an option to allow unified training across different datasets (Spider/Sparc/CoSQL)

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -27,19 +27,30 @@ class Preprocessor:
 
     def preprocess(self, sections, keep_vocab):
         self.model_preproc.clear_items()
-        for section in sections:
-            data = registry.construct("dataset",
-                                      self.config["data"][section])  # SpiderDataset/SparcDataset/CoSQLDataset
-            sample_size = self.config["data"].get(f'{section}_sample_size', None)
-            if section in 'train_sample_ratio' and 'train_sample_ratio' in self.config["data"]:
-                sample_size = int(len(data) * float(self.config["data"]['train_sample_ratio'] / 100))
-            data.sample(sample_size=sample_size)
-            for i, item in enumerate(tqdm.tqdm(data, desc=section, dynamic_ncols=True)):  # SpiderItem/SparcItem
-                to_add, validation_info = self.model_preproc.validate_item(
-                    item, section
-                )
-                if to_add:
-                    self.model_preproc.add_item(item, section, validation_info)
+
+        if isinstance(self.config["data"], dict):
+            datasets = [self.config["data"]]
+        elif isinstance(self.config["data"], list):
+            datasets = self.config["data"]
+        else:
+            raise ValueError(f"Error in config.data, either a list or dictionary.")
+
+        for dataset in datasets:
+            if 'name' in dataset:
+                print(f"Processing dataset {dataset['name']}...")
+            for section in sections:
+                data = registry.construct("dataset",
+                                          dataset[section])  # SpiderDataset/SparcDataset/CoSQLDataset
+                sample_size = self.config["data"].get(f'{section}_sample_size', None)
+                if section in 'train_sample_ratio' and 'train_sample_ratio' in self.config["data"]:
+                    sample_size = int(len(data) * float(self.config["data"]['train_sample_ratio'] / 100))
+                data.sample(sample_size=sample_size)
+                for i, item in enumerate(tqdm.tqdm(data, desc=section, dynamic_ncols=True)):  # SpiderItem/SparcItem
+                    to_add, validation_info = self.model_preproc.validate_item(
+                        item, section
+                    )
+                    if to_add:
+                        self.model_preproc.add_item(item, section, validation_info)
         if keep_vocab:
             self.model_preproc.save_examples()
         else:
