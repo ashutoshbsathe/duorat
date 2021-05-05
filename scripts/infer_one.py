d5f93e3ebe68eff9a7ceb8f8be7873ff65252a4b
Cong Duy Vu Hoang <vu.hoang@oracle.com>
2021-05-06 09:33:30 +1000

    Fix scripts/infer_one.py

--- scripts/infer_one.py
+++ scripts/infer_one.py
@@ -18,6 +18,8 @@ if __name__ == "__main__":
                         help="The configuration file. By default, an arbitrary configuration from the logdir is loaded")
     parser.add_argument("--db-folder-path",
                         help="The folder path to DB folder")
+    parser.add_argument("--data-type", type=str, default='Spider', required=False,
+                        help="The data type, e.g., Spider, Sparc, CoSQL")
     parser.add_argument(
         "--eval-file", required=True,
         help="The path to the evaluation file in JSON"
@@ -40,36 +42,57 @@ if __name__ == "__main__":
     with open(args.eval_file) as f:
         eval_data = json.load(f)
         for data_example in eval_data:
-            db_path = f"{os.path.join(args.db_folder_path, data_example['db_id'])}" + f"/{data_example['db_id']}.sqlite"
-            schema_path = f"{os.path.join(args.db_folder_path, data_example['db_id'], 'tables.json')}"
+            db_id = 'db_id' if args.data_type == 'Spider' else 'database_id'
+            db_path = f"{os.path.join(args.db_folder_path, data_example[db_id])}" + f"/{data_example[db_id]}.sqlite"
+            schema_path = f"{os.path.join(args.db_folder_path, data_example[db_id], 'tables.json')}"
+            if not os.path.exists(schema_path):
+                schema_path = ''
+
             if os.path.exists(db_path) or os.path.exists(schema_path):
                 infer_time = time.perf_counter()
                 duorat_on_db = DuoratOnDatabase(duorat_api,
                                                 db_path,
                                                 schema_path)
-                question = data_example['question']
-                results = duorat_on_db.infer_query(question)
-                infer_time = time.perf_counter() - infer_time
-                total_infer_time += infer_time
 
-                print("-" * 20)
-                print(pretty_format_slml(results['slml_question']))
-                print(f'{results["query"]}  ({results["score"]})')
-                print("-" * 20)
+                interactions = []
+                if 'interaction' in data_example:
+                    for interaction in data_example['interaction']:
+                        interactions.append((interaction['utterance'], interaction['query']))
+                else:
+                    interactions.append((data_example['question'], ''))
 
-                if args.do_execute:
-                    try:
-                        exe_time = time.perf_counter()
-                        results = duorat_on_db.execute(results['query'])
-                        exe_time = time.perf_counter() - exe_time
-                        total_exe_time += exe_time
+                for index, interaction in enumerate(interactions):
+                    history = None
+                    if index > 0:
+                        if duorat_api.config['model']['preproc']['interaction_type'] == 'source':
+                            history = [interactions[index - 1][0]]
+                        elif duorat_api.config['model']['preproc']['interaction_type'] == 'target':
+                            history = [interactions[index - 1][1]]
 
-                        print(results)
-                    except Exception as e:
-                        print(str(e))
+                    question = interaction[0]
 
-                # release memory if required
-                del duorat_on_db
+                    results = duorat_on_db.infer_query(question, history=history)
+                    infer_time = time.perf_counter() - infer_time
+                    total_infer_time += infer_time
+
+                    print("-" * 20)
+                    print(pretty_format_slml(results['slml_question']))
+                    print(f'{results["query"]}  ({results["score"]})')
+                    print("-" * 20)
+
+                    if args.do_execute:
+                        try:
+                            exe_time = time.perf_counter()
+                            results = duorat_on_db.execute(results['query'])
+                            exe_time = time.perf_counter() - exe_time
+                            total_exe_time += exe_time
+
+                            print(results)
+                        except Exception as e:
+                            print(str(e))
+
+                    # release memory if required
+                    del duorat_on_db
 
         num_examples = len(eval_data)
         print(f"Loading time: {load_time} secs")
--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -340,5 +340,7 @@ CUDA_VISIBLE_DEVICES=3 python scripts/infer.py --config configs/duorat/duorat-sp
 CUDA_VISIBLE_DEVICES=0 python scripts/infer_one.py --config configs/duorat/duorat-new-db-content.jsonnet --logdir ./logdir/duorat-new-db-content-bs4-ac7 --db-folder-path ./data/database/ --eval-file ./data/spider/dev.json
 
 # Sparc
+CUDA_VISIBLE_DEVICES=0 python scripts/infer_one.py --config configs/duorat/duorat-sparc-new-db-content.jsonnet --logdir ./logdir/duorat-sparc-new-db-content --db-folder-path ./data/sparc/database/ --eval-file ./data/sparc/dev.json
 
 # CoSQL
+CUDA_VISIBLE_DEVICES=0 python scripts/infer_one.py --config configs/duorat/duorat-cosql-new-db-content.jsonnet --logdir ./logdir/duorat-cosql-new-db-content --db-folder-path ./data/sparc/database/ --eval-file ./data/sparc/dev.json
