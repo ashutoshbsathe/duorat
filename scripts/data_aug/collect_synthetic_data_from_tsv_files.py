d895f1a02a76204f82b582cc1ef827c372a249a1
Cong Duy Vu Hoang <vu.hoang@oracle.com>
2021-05-17 17:53:45 +1000

    Add samples_by_level option for scripts/data_aug/collect_synthetic_data_from_tsv_files.py

--- scripts/data_aug/collect_synthetic_data_from_tsv_files.py
+++ scripts/data_aug/collect_synthetic_data_from_tsv_files.py
@@ -1,12 +1,14 @@
 import argparse
 import glob
 import json
+from sklearn import model_selection
 
 
-def collect_synthetic_data(tsv_files_folder_path: str, output_data_file):
+def collect_synthetic_data(tsv_files_folder_path: str, output_data_file, samples_by_level: int = 100):
     output_data = []
     for file_name in glob.iglob(f'{tsv_files_folder_path}/**/*.tsv', recursive=True):
         with open(file_name) as f:
+            examples = []
             for line in f:
                 line = line.strip()
                 _, question, query = line.split('\t')
@@ -20,8 +22,12 @@ def collect_synthetic_data(tsv_files_folder_path: str, output_data_file):
                     'question_toks': '',
                     'sql': ''
                 }
+                examples.append(example_dict)
 
-                output_data.append(example_dict)
+            sampled_examples, _ = model_selection.train_test_split(examples,
+                                                                   random_state=42,
+                                                                   train_size=samples_by_level)
+            output_data.extend(sampled_examples)
 
     with open(output_data_file, 'w') as outf:
         json.dump(output_data, outf, indent=4, sort_keys=True)
@@ -31,7 +37,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--tsv-files-folder-path", required=True)
     parser.add_argument("--output-data-file", required=True)
+    parser.add_argument("--samples-by-level", type=int, default=100, required=False)
     args = parser.parse_args()
 
     collect_synthetic_data(tsv_files_folder_path=args.tsv_files_folder_path,
-                           output_data_file=args.output_data_file)
+                           output_data_file=args.output_data_file,
+                           samples_by_level=args.samples_by_level)
