03e6ea2f066aac0a0dbc62cb6667f5bad28e59e1
Cong Duy Vu Hoang <vu.hoang@oracle.com>
2021-07-12 22:22:46 +1000

    Add TabFact

--- scripts/focusing/collect_data.py
+++ scripts/focusing/collect_data.py
@@ -81,7 +81,35 @@ def collect_wikitablequestions(dataset_path: str, tsv_data_file_name: str, outpu
             output_data.append(' '.join(concat_output))
 
     with open(output_file, 'w') as outf:
-        for entry in output_data:
+        for entry in tqdm.tqdm(output_data):
+            outf.write(f"{entry}\n")
+
+    return
+
+
+def collect_tabfact(tsv_data_file: str, data_path: str, output_file: str) -> None:
+    tsv_data_file_path = os.path.join(data_path, tsv_data_file)
+    csv_tables_folder_path = os.path.join(data_path, 'data/all_csv')
+    output_data = []
+    with open(tsv_data_file_path) as f:
+        for line in f:
+            line = line.strip()
+            parts = line.split('\t')
+            text = parts[-1]
+            csv_tables_file = os.path.join(csv_tables_folder_path, parts[0])
+            with open(csv_tables_file) as tf:
+                header_line = tf.readline()
+                col_list = header_line.split('#')
+
+            concat_output = [text]
+            for col in col_list:
+                concat_output.append('</s>')
+                concat_output.append(col)
+
+            output_data.append(' '.join(concat_output))
+
+    with open(output_file, 'w') as outf:
+        for entry in tqdm.tqdm(output_data):
             outf.write(f"{entry}\n")
 
     return
@@ -100,3 +128,7 @@ if __name__ == "__main__":
     collect_wikitablequestions(dataset_path='../../data/focusing/non_sql_tabular_datasets/WikiTableQuestions',
                                tsv_data_file_name='data/training.tsv',
                                output_file='./data/focusing/wikitablequestions.txt')
+
+    collect_tabfact(tsv_data_file='processed_datasets/tsv_data_horizontal/tabfact_all_data.tsv',
+                    data_path='../../data/focusing/non_sql_tabular_datasets/Table-Fact-Checking',
+                    output_file='./data/focusing/tabfact.txt')
