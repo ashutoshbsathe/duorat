8d7b7d86204d22f009c2443f765ac32fdb1e9f6e
Cong Duy Vu Hoang <vu.hoang@oracle.com>
2021-07-13 17:14:56 +1000

    Fix

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -598,8 +598,9 @@ CUDA_VISIBLE_DEVICES=0 python scripts/train.py --config configs/duorat/duorat-sp
 python scripts/data_aug/collect_synthetic_data_template_scfg.py --file-type json --files-folder-path /mnt/shared/parthur/experiments/nl2sql/output/data/databases/v5_mono/spider/database --output-data-file ./data/spider/spider_all_dbs_synthetic_data_v5_mono_nl_by_t5_gen_full.json --samples-by-db -1
 python3 scripts/split_spider_by_db.py --examples-paths 'spider_all_dbs_synthetic_data_v5_mono_nl_by_t5_gen_full.json' --default-example-file-name examples_with_synthetic_data_v5_mono_nl_by_t5_gen_full.json
 
-# * train with mix of original and synthetic data with batch balancing
-CUDA_VISIBLE_DEVICES=0 python scripts/train.py --config configs/duorat/duorat-spider-new-db-content-with-pretrained-embeddings-electra-base-150k-steps-train-mix-bb-original-plus-synthetic-data-by-gold-template-scfg-alls.jsonnet --logdir ./logdir/duorat-spider-new-db-content-with-pretrained-embeddings-electra-base-150k-steps-train-mix-bb-original-plus-synthetic-data-by-gold-template-scfg-alls --force-preprocess --force-train
+# w/ world_1 only
+# w/ batch balancing
+CUDA_VISIBLE_DEVICES=0 python scripts/train.py --config configs/duorat/duorat-spider-new-db-content-with-pretrained-embeddings-electra-base-150k-steps-initialized-train-plus-dev-synthetic-data-v5-mono-nl-by-gen-200s-bb.jsonnet --logdir ./logdir/duorat-spider-new-db-content-with-pretrained-embeddings-electra-base-150k-steps-initialized-train-plus-dev-synthetic-data-v5-mono-nl-by-gen-200s-bb --force-preprocess --force-train
 
 # 1-shot
 python scripts/data_aug/collect_synthetic_data_template_scfg.py --file-type json --files-folder-path /mnt/shared/parthur/experiments/nl2sql/output/data/v3_fixed/database --output-data-file ./data/spider/train_synthetic_data_by_template_scfg_v3_fixed_val_db_only_1shot.json --samples-by-db 1
